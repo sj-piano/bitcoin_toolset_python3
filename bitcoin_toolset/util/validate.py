7dffb9a30f3de6a05909e8566a986b185b4e0f1c
StJohn Piano <stjohnpiano@gmail.com>
2022-10-23 09:40:58 +0200

    Expand validate.py

--- bitcoin_toolset/util/validate.py
+++ bitcoin_toolset/util/validate.py
@@ -15,6 +15,8 @@ import re
 
 
 
+
+
 # ### SECTION
 # Components.
 
@@ -25,8 +27,94 @@ hex_digits = '0123456789abcdef'
 
 
 
+
+
 # ### SECTION
 # Some more complex validation functions
+
+
+
+
+def validate_list_contains_items(input, items):
+  # input is a list. items is a list.
+  validate_list(input)
+  validate_list(items)
+  not_found = []
+  for x in items:
+    if x not in input:
+      not_found.append(x)
+  if len(not_found) == 0:
+    return True
+  msg = "Some items are not in the list."
+  msg += "\nList items:"
+  for x in input:
+    msg += "\n- %s" % str(x)
+  msg += "\nItems not found:"
+  for x in not_found:
+    msg += "\n- %s" % str(x)
+  raise ValueError(msg)
+
+
+
+
+def validate_item_in_list(input, list1):
+  validate_list(list1)
+  if input in list1:
+    return True
+  msg = "Item not in list. Item: {}".format(input)
+  msg += "\nList items:"
+  for item in list1:
+    msg += "\n- {}".format(item)
+  raise ValueError(msg)
+
+
+
+
+def validate_lists_are_identical(list1, list2):
+  if sorted(list1) != sorted(list2):
+    msg = '''
+List are not identical.
+- List 1:
+{}
+- List 2:
+{}
+'''
+    msg = msg.format(list1, list2)
+    raise ValueError(msg)
+
+
+
+
+def validate_integer_domain(n, min_value, max_value):
+  # n must be within [min_value, max_value].
+  validate_integer(n)
+  validate_integer(min_value)
+  validate_integer(max_value)
+  if min_value > max_value:
+    msg = '''
+Minimum value must be less than maximum value.
+- Mininimum value: {}
+- Maximum value: {}
+'''.format(min_value, max_value)
+    raise ValueError(msg)
+  if (min_value <= n <= max_value):
+    return
+  elif n < min_value:
+    msg = '''
+Input is less than the minimum value.
+- Minimum value: {}
+- Input: {}
+'''.format(min_value, n)
+    raise ValueError(msg)
+  elif n > max_value:
+    msg = '''
+Input is greater than the maximum value.
+- Maximum value: {}
+- Input: {}
+'''.format(max_value, n)
+    raise ValueError(msg)
+
+
 
 
 def validate_string_is_printable_ascii(x, name=None, location=None, kind='string_is_printable_ascii'):
@@ -49,6 +137,61 @@ def validate_string_is_printable_ascii(x, name=None, location=None, kind='string
     if c == '\n':
       line += 1
       index = 0
+
+
+
+
+def validate_string_is_whole_number_or_decimal(
+    s, dp=None, name=None, location=None, kind='integer',
+  ):
+  # 's' only needs to pass one check out of the two.
+  stored_exceptions = []
+  success = 0
+  try:
+    validate_string_is_whole_number(s, name, location, kind)
+    success = 1
+  except Exception as e1:
+    stored_exceptions.append(e1)
+  try:
+    validate_string_is_decimal(s, dp, name, location, kind)
+    success = 1
+  except Exception as e2:
+    stored_exceptions.append(e2)
+  if not success:
+    e = stored_exceptions[0]
+    raise e
+
+
+
+
+def validate_string_is_decimal(
+    s, dp=None, name=None, location=None, kind='integer',
+    ):
+  # dp = decimal places
+  validate_string(s, name, location, kind)
+  if s.count('.') != 1:
+    msg = "which has {} decimal points, not 1.".format(s.count('.'))
+    msg = build_error_msg(msg, s, name, location=location, kind=None)
+    raise TypeError(msg)
+  s1, s2 = s.split('.')
+  if not s1.isdigit() or not s2.isdigit():
+    msg = "which has non-digit characters."
+    msg = build_error_msg(msg, s, name, location=location, kind=None)
+    raise TypeError(msg)
+  # Check if an exact number of decimal places has been specified.
+  if isinstance(dp, int):
+    regex = r'^\d*.\d{%d}$' % dp
+    decimal_pattern = re.compile(regex)
+    if not decimal_pattern.match(s):
+      msg = 'which is not a valid {}-decimal-place decimal value.'.format(dp)
+      msg = build_error_msg(msg, s, name, location, kind)
+      raise ValueError(msg)
+
+
+sd = validate_string_is_decimal
+
+
+
 
 
 
@@ -110,6 +253,8 @@ def validate_positive_integer(n, name=None, location=None, kind='positive_intege
 pi = validate_positive_integer
 
 
+
+
 def validate_integer(n, name=None, location=None, kind='integer'):
   if not isinstance(n, int):
     msg = "which has type '{}', not 'int'.".format(type(n).__name__)
@@ -120,6 +265,8 @@ def validate_integer(n, name=None, location=None, kind='integer'):
 i = validate_integer
 
 
+
+
 def validate_boolean(b, name=None, location=None, kind='boolean'):
   if type(b) != bool:
     msg = "which has type '{}', not 'bool'.".format(type(b).__name__)
@@ -128,6 +275,8 @@ def validate_boolean(b, name=None, location=None, kind='boolean'):
 
 
 b = validate_boolean
+
+
 
 
 def validate_hex_length(s, n, name=None, location=None, kind=None):
@@ -157,25 +306,6 @@ def validate_hex(s, name=None, location=None, kind='hex'):
     raise ValueError(msg)
 
 
-def validate_string_is_decimal(
-    s, dp=2, name=None, location=None, kind='integer',
-    ):
-  # dp = decimal places
-  string(s, name, location, kind)
-  if not isinstance(dp, int):
-    msg = "which has type '{}', not 'int'.".format(type(dp).__name__)
-    name2 = 'dp (i.e. the number of decimal places)'
-    msg = build_error_msg(msg, dp, name=name2, location=location, kind=None)
-    raise TypeError(msg)
-  regex = r'^\d*.\d{%d}$' % dp
-  decimal_pattern = re.compile(regex)
-  if not decimal_pattern.match(s):
-    msg = 'which is not a valid {}-decimal-place decimal value.'.format(dp)
-    msg = build_error_msg(msg, s, name, location, kind)
-    raise ValueError(msg)
-
-
-sd = validate_string_is_decimal
 
 
 def validate_string_is_whole_number(
@@ -189,6 +319,8 @@ def validate_string_is_whole_number(
 
 
 swn = validate_string_is_whole_number
+
+
 
 
 def validate_string_is_positive_integer(
@@ -209,6 +341,8 @@ def validate_string_is_positive_integer(
 spi = validate_string_is_positive_integer
 
 
+
+
 def validate_string_is_date(s, name=None, location=None, kind='string_is_date'):
   validate_string(s, name, location, kind)
   if not date_pattern.match(s):
@@ -218,6 +352,8 @@ def validate_string_is_date(s, name=None, location=None, kind='string_is_date'):
 
 
 sdate = validate_string_is_date
+
+
 
 
 def validate_string(s, name=None, location=None, kind='string'):
@@ -252,4 +388,6 @@ def build_error_msg(msg, value, name=None, location=None, kind=None):
     m += ', ' + msg
   m = m[0].capitalize() + m[1:]
   return m
+
+
 
